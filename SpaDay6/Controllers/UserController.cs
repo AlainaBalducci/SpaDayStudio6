536badc8e78886a21d54d5327046c3599d5270a7
Alaina Balducci <alainabalducci@gmail.com>
2023-03-28 14:40:27 -0500

    started studio

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.Username = newUser.Username;
+                return View("/Index");
+            }
+            else
+            {
+                return View("/user/add");
+            }
+        }
+
+    }
+}
