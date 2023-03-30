526b6bdab435b5f214c2991f50627495ff4d70ef
Alaina Balducci <alainabalducci@gmail.com>
2023-03-30 15:39:50 -0500

    Finished chapter 15 studio

---
+++ SpaDay6/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay6.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Please enter your username")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Your username must be between 5 and 15 characters long")]
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Your password must be between 6 and 20 characters long")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please verify your password")]
+        public string? VerifyPassword { get; set; }
+    }
+}
